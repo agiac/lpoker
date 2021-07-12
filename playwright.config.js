264796c9c55f403895f7b84511843cf6c3534f75
Alberto Giachino <alberto.giachino@limango.com>
2021-07-12 23:43:13 +0200

    Home page bahviour

--- playwright.config.js
+++ playwright.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   globalSetup: require.resolve("./tests/globalSetup.js"),
+  timeout: 5000,
 };
--- tests/homePage.spec.js
+++ tests/homePage.spec.js
@@ -1,14 +1,23 @@
 const { test, expect } = require("@playwright/test");
 
 test.describe("The home page", () => {
-  test("should display the app name with an h1 tag", async ({ page }) => {
-    await page.goto(`http://localhost:${process.env.SERVER_PORT}`);
+  const homePage = `http://localhost:${process.env.SERVER_PORT}`;
 
-    const titleTag = await page.$eval(
-      "text=LPoker",
-      (element) => element.tagName
-    );
+  test("should display the app name", async ({ page }) => {
+    await page.goto(homePage);
 
-    expect(titleTag).toBe("H1");
+    const title = await page.$("text=LPoker");
+
+    expect(title).not.toBeNull();
+  });
+
+  test("should allow to create and join a new room", async ({ page }) => {
+    await page.goto(homePage);
+
+    await page.click("text=Create new room");
+
+    await page.click("text=Room created: follow me");
+
+    await page.waitForURL(`${homePage}/**`);
   });
 });
