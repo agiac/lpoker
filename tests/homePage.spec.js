7e2c28a550cfef27b6e74c82ea3331005afc5633
Alberto Giachino <alberto.giachino@limango.com>
2021-07-27 12:32:00 +0200

    Update playwright

--- tests/homePage.spec.js
+++ tests/homePage.spec.js
@@ -6,7 +6,9 @@ const { test } = require("@playwright/test");
 test.describe("The home page", () => {
   const homePage = `http://localhost:${process.env.SERVER_PORT}`;
 
-  test("should redirect to the desired room", async ({ page }) => {
+  test("when the 'JOIN' button is clicked, it should redirect to the desired room", async ({
+    page,
+  }) => {
     const roomId = humanId();
 
     await page.goto(homePage);
